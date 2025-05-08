0ff4c4f34ed6e3e5d045e81433858fd1f2bac7e2
ccollie <gboyah@gmail.com>
2025-05-07 22:24:11 -0400

    Better deserialization error handling.

--- src/fanout/results_tracker.rs
+++ src/fanout/results_tracker.rs
@@ -1,8 +1,6 @@
 use enum_dispatch::enum_dispatch;
 use std::any::Any;
-use std::collections::BinaryHeap;
-use std::sync::{Arc, Mutex, MutexGuard};
-use valkey_module::{ValkeyError, ValkeyResult};
+use std::sync::{Mutex, MutexGuard};
 
 pub type ResponseCallback<T> = Box<dyn FnOnce(Vec<T>, Vec<String>) + Send>;
 
@@ -108,111 +106,5 @@ impl<T: 'static> ResponseTracker<T> for ResultsTracker<T> {
 
     fn raise_error(&self, error: &str) {
         self.raise_error(error)
-    }
-}
-
-struct TopKResultsTrackerInner<T> {
-    data: BinaryHeap<T>,
-    errors: Vec<String>,
-    callback: Option<ResponseCallback<T>>,
-    outstanding_requests: u32,
-}
-
-pub struct TopKResultsTracker<T: PartialOrd> {
-    inner: Mutex<TopKResultsTrackerInner<T>>,
-    max_results: usize, // Maximum number of results to keep
-}
-
-impl<T: PartialOrd + Ord> TopKResultsTracker<T> {
-    pub fn new(
-        outstanding_requests: u32,
-        max_results: usize,
-        callback: ResponseCallback<T>,
-    ) -> Self {
-        let data = BinaryHeap::with_capacity(max_results);
-        let inner = TopKResultsTrackerInner {
-            data,
-            errors: Vec::new(),
-            callback: Some(callback),
-            outstanding_requests,
-        };
-        Self {
-            inner: Mutex::new(inner),
-            max_results,
-        }
-    }
-
-    pub fn add_internal(&self, result: T, data: &mut BinaryHeap<T>) {
-        if data.len() < self.max_results {
-            data.push(result);
-        } else if let Some(top) = data.peek() {
-            if result < *top {
-                data.pop();
-                data.push(result);
-            }
-        }
-    }
-
-    pub fn update(&self, result: T) -> bool {
-        let mut inner = self.inner.lock().unwrap();
-        self.add_internal(result, &mut inner.data);
-        self.decrement_internal(&mut inner)
-    }
-
-    /// This is used only in the case of a node failure, so that the callback is still called
-    /// when the last request is received from the remaining nodes.
-    pub fn decrement(&self) -> bool {
-        let mut inner = self.inner.lock().unwrap();
-        self.decrement_internal(&mut inner)
-    }
-
-    fn decrement_internal(&self, inner: &mut MutexGuard<TopKResultsTrackerInner<T>>) -> bool {
-        inner.outstanding_requests = inner.outstanding_requests.saturating_sub(1);
-        // If no outstanding request, execute the callback
-        if inner.outstanding_requests == 0 {
-            if let Some(callback) = inner.callback.take() {
-                let final_results: Vec<T> = inner.data.drain().collect();
-                let errors: Vec<String> = std::mem::take(&mut inner.errors);
-                callback(final_results, errors);
-                return true;
-            }
-        }
-        false
-    }
-
-    pub fn raise_error(&self, error: &str) {
-        let mut inner = self.inner.lock().unwrap();
-        inner.outstanding_requests = inner.outstanding_requests.saturating_sub(1);
-        inner.errors.push(error.to_string());
-        if inner.outstanding_requests == 0 {
-            if let Some(callback) = inner.callback.take() {
-                let final_results: Vec<T> = inner.data.drain().collect();
-                let errors: Vec<String> = std::mem::take(&mut inner.errors);
-                callback(final_results, errors);
-            }
-        }
-    }
-
-    pub fn completed(&self) -> bool {
-        let inner = self.inner.lock().unwrap();
-        inner.outstanding_requests == 0
-    }
-}
-
-impl<T: PartialOrd + 'static + Ord> ResponseTracker<T> for TopKResultsTracker<T> {
-    fn update(&self, result: T) -> bool {
-        TopKResultsTracker::update(self, result)
-    }
-
-    fn decrement(&self) -> bool {
-        TopKResultsTracker::decrement(self)
-    }
-
-    fn completed(&self) -> bool {
-        TopKResultsTracker::completed(self)
-    }
-
-    fn raise_error(&self, error: &str) {
-        TopKResultsTracker::raise_error(self, error)
     }
 }
