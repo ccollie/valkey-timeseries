ab663a6c089d19643c68bac2190fd992e47858c3
ccollie <gboyah@gmail.com>
2025-05-17 21:33:38 -0400

    Added `FilteredSampleIterator` to enable sample filtering based on value and timestamp conditions. Integrated it into `TimeSeriesChunk` with a `filtered_iter` method for streamlined filtering in range queries.

--- src/iterators/mod.rs
+++ src/iterators/mod.rs
@@ -1,8 +1,10 @@
+mod filtered_sample_iterator;
 mod multi_series_sample_iter;
 mod sample_iter;
 mod sample_merge_iterator;
 mod vec_sample_iterator;
 
+pub use filtered_sample_iterator::FilteredSampleIterator;
 pub use multi_series_sample_iter::MultiSeriesSampleIter;
 pub use sample_iter::SampleIter;
 pub use sample_merge_iterator::SampleMergeIterator;
