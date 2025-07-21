1de25375fb0245a301669374831fbfa1b4a5d083
ccollie <gboyah@gmail.com>
2025-07-21 10:55:20 -0400

    feature: support `LATEST` option for `TS.GET` to get the value of the latest aggregation in a compaction.

--- src/commands/get.rs
+++ src/commands/get.rs
@@ -1,33 +1,37 @@
 use crate::series::{get_latest_compaction_sample, with_timeseries};
 use valkey_module::ValkeyError::WrongArity;
-use valkey_module::{Context, NextArg, ValkeyError, ValkeyResult, ValkeyString, ValkeyValue};
+use valkey_module::{Context, ValkeyError, ValkeyResult, ValkeyString, ValkeyValue};
 
 /// TS.GET key [LATEST]
 pub fn get(ctx: &Context, args: Vec<ValkeyString>) -> ValkeyResult {
-    let mut args = args.into_iter().skip(1);
-    if let Ok(key) = args.next_arg() {
-        let latest = match args.next_arg() {
-            Ok(s) if s.eq_ignore_ascii_case("latest".as_ref()) => true,
-            Ok(s) => {
-                let msg = format!("TSDB: invalid argument \"{s}\"");
-                return Err(ValkeyError::String(msg));
-            }
-            _ => return Err(WrongArity),
-        };
-        args.done()?;
-
-        let sample = with_timeseries(ctx, &key, true, move |series| {
-            Ok(if latest {
-                get_latest_compaction_sample(ctx, series)
-            } else {
-                series.last_sample
-            })
-        })?;
-        Ok(match sample {
-            Some(sample) => sample.into(),
-            None => ValkeyValue::Array(vec![]),
-        })
-    } else {
-        Err(WrongArity)
+    if args.len() < 2 || args.len() > 3 {
+        return Err(WrongArity);
     }
+
+    let latest = if args.len() == 3 {
+        let arg = args[2].as_slice();
+        if arg.len() != 6 || !arg.eq_ignore_ascii_case("latest".as_ref()) {
+            return Err(ValkeyError::String(format!(
+                "TSDB: invalid argument \"{}\"",
+                args[2]
+            )));
+        }
+        true
+    } else {
+        false
+    };
+
+    let key = &args[1];
+    let sample = with_timeseries(ctx, key, true, move |series| {
+        Ok(if latest {
+            get_latest_compaction_sample(ctx, series)
+        } else {
+            series.last_sample
+        })
+    })?;
+
+    Ok(match sample {
+        Some(sample) => sample.into(),
+        None => ValkeyValue::Array(vec![]),
+    })
 }
