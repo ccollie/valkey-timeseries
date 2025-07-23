e403af65ea6360547bda6743f820358e536c326e
ccollie <gboyah@gmail.com>
2025-07-22 20:38:26 -0400

    refactor: update SampleAddResult to use Sample instead of Timestamp

--- src/series/chunks/pco/pco_chunk.rs
+++ src/series/chunks/pco/pco_chunk.rs
@@ -701,6 +701,7 @@ mod tests {
     use crate::series::{DuplicatePolicy, SampleAddResult};
     use crate::tests::generators::DataGenerator;
     use std::time::Duration;
+    use crate::commands::add;
 
     fn decompress(chunk: &PcoChunk) -> Vec<Sample> {
         chunk.iter().collect()
