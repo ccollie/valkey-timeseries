6758e953c7ce57146ebb152b7bec3420e574b296
ccollie <gboyah@gmail.com>
2025-10-08 10:47:20 -0400

    docs: update comments for querier_tests.rs

--- src/series/index/querier_tests.rs
+++ src/series/index/querier_tests.rs
@@ -1,18 +1,13 @@
-// Copyright 2017 The Prometheus Authors
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// https://github.com/prometheus/prometheus/blob/main/tsdb/index/postings_test.go
-// https://github.com/prometheus/prometheus/blob/main/tsdb/querier_test.go (see func TestPostingsForMatchers())
+//! Tests for querying postings from the time series index.
+//! Specifically, these tests verify that the correct postings are returned for a given set of
+//! Prometheus style series selectors. These tests are heavily inspired by Prometheus's own
+//! querier tests.
+//!
+//! https://github.com/prometheus/prometheus/blob/main/tsdb/index/postings_test.go
+//! https://github.com/prometheus/prometheus/blob/main/tsdb/querier_test.go (see func TestPostingsForMatchers())
+//!
+//! Original Code Copyright 2017 The Prometheus Authors
+//! Licensed under the Apache License, Version 2.0 (the "License");
 #[cfg(test)]
 mod tests {
     use crate::labels::matchers::{MatchOp, Matcher, Matchers};
