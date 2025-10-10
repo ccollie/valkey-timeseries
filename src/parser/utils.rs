1874020bcffd2388275ab977ed8632fbdbeec24d
ccollie <gboyah@gmail.com>
2025-10-10 10:18:55 -0400

    minor clippy fixes.

--- src/parser/utils.rs
+++ src/parser/utils.rs
@@ -12,13 +12,13 @@ const fn is_ident_char(ch: char) -> bool {
     matches!(ch, 'A'..='Z' | 'a'..='z' | '0'..='9' | '_' | ':' | '.')
 }
 
-/// interprets `token` as a single-quoted, double-quoted, or backquoted
+/// Interprets `token` as a single-quoted, double-quoted, or backquoted
 /// Prometheus query language string literal, returning the string value that `token`
 /// quotes.
 ///
-/// Special-casing for single quotes was removed and single quoted strings are now treated the
+/// Special-casing for single quotes was removed, and single quoted strings are now treated the
 /// same as double-quoted ones.
-pub fn extract_string_value(token: &str) -> ParseResult<Cow<str>> {
+pub fn extract_string_value(token: &'_ str) -> ParseResult<Cow<'_, str>> {
     let n = token.len();
 
     if n < 2 {
@@ -107,7 +107,7 @@ pub fn escape_ident(s: &str) -> String {
     dst.to_string()
 }
 
-pub fn unescape_ident(s: &str) -> ParseResult<Cow<str>> {
+pub fn unescape_ident(s: &'_ str) -> ParseResult<Cow<'_, str>> {
     let v = s.find('\\');
     if v.is_none() {
         return Ok(Cow::Borrowed(s));
