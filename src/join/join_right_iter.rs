7a1593ba673f484bc28a21937ef826ac8b3f21a9
ccollie <gboyah@gmail.com>
2026-01-06 11:47:48 -0500

    more efficient iteration of JoinRightIter

--- src/join/join_right_iter.rs
+++ src/join/join_right_iter.rs
@@ -1,10 +1,14 @@
-use crate::common::Sample;
+use crate::common::{Sample, Timestamp};
 use crate::join::JoinValue;
 use joinkit::{EitherOrBoth, Joinkit};
 use min_max_heap::MinMaxHeap;
 
+/// Iterator for right outer join.
+/// INVARIANT: assumes that both iterators are sorted by timestamp.
+#[must_use = "iterator adaptors are lazy and do nothing unless consumed"]
 pub(super) struct JoinRightIter {
-    loaded: bool,
+    exhausted: bool,
+    prev_ts: Timestamp,
     heap: MinMaxHeap<JoinValue>,
     inner: Box<dyn Iterator<Item = EitherOrBoth<Sample, Vec<Sample>>>>,
 }
@@ -20,19 +24,15 @@ impl JoinRightIter {
         let left_iter = left.into_iter().map(|sample| (sample.timestamp, sample));
         let right_iter = right.into_iter().map(|sample| (sample.timestamp, sample));
 
-        // estimate capacity
-        let capacity = right_iter.size_hint().0;
-        let heap = if capacity > 0 {
-            // pre-allocate heap with estimated capacity
-            MinMaxHeap::with_capacity(capacity)
-        } else {
-            MinMaxHeap::new()
-        };
-
+        // For each right timestamp, we can have multiple samples grouped into a Vec<Sample>.
+        // The heap will store JoinValue items to be returned in sorted order. We allocate a
+        // reasonable initial capacity for the heap.
+        let heap = MinMaxHeap::with_capacity(4);
         let iter = left_iter.hash_join_right_outer(right_iter);
 
         Self {
-            loaded: false,
+            exhausted: false,
+            prev_ts: -1,
             inner: Box::new(iter),
             heap,
         }
@@ -64,12 +64,32 @@ impl Iterator for JoinRightIter {
     type Item = JoinValue;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if !self.loaded {
-            self.loaded = true;
-            while let Some(item) = self.inner.next() {
-                self.push_item(item)
+        loop {
+            // If we have items in the heap, return the smallest one.
+            if let Some(value) = self.heap.pop_min() {
+                return Some(value);
+            }
+            // If the heap is empty, and we've exhausted the inner iterator,
+            // there are no more items to produce.
+            if self.exhausted {
+                return None;
+            }
+
+            // iterate until we get a new timestamp greater than prev_ts
+            while let Some(value) = self.inner.next() {
+                self.push_item(value);
+                if let Some(peek) = self.heap.peek_min() {
+                    let ts = peek.sortable_timestamp();
+                    if ts > self.prev_ts && self.prev_ts >= 0 {
+                        self.prev_ts = ts;
+                        break;
+                    }
+                }
+            }
+
+            if self.heap.is_empty() {
+                self.exhausted = true;
             }
         }
-        self.heap.pop_min()
     }
 }
