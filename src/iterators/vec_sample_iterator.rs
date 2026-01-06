aaab7c07941a54c450da4efc8689222625db91d7
ccollie <gboyah@gmail.com>
2026-01-06 11:45:40 -0500

    refactor(vec_sample_iterator): implement size_hint and ExactSizeIterator

--- src/iterators/vec_sample_iterator.rs
+++ src/iterators/vec_sample_iterator.rs
@@ -2,13 +2,16 @@ use crate::common::Sample;
 use std::vec::IntoIter;
 
 pub struct VecSampleIterator {
+    len: usize,
     inner: IntoIter<Sample>,
 }
 
 impl VecSampleIterator {
     pub fn new(samples: Vec<Sample>) -> Self {
+        let len = samples.len();
         let inner = samples.into_iter(); // slice iterator
         Self {
+            len,
             inner, // slice iterator
         }
     }
@@ -19,5 +22,16 @@ impl Iterator for VecSampleIterator {
 
     fn next(&mut self) -> Option<Self::Item> {
         self.inner.next()
+    }
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len = self.len;
+        (len, Some(len))
+    }
+}
+
+// implement size_hint for VecSampleIterator
+impl ExactSizeIterator for VecSampleIterator {
+    fn len(&self) -> usize {
+        self.len
     }
 }
