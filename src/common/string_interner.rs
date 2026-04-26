e8c878eda9537992a930db2da4e63a03a02135b3
ccollie <gboyah@gmail.com>
2026-04-25 23:35:46 -0400

    fix: make doc tests non-executable

--- src/common/string_interner.rs
+++ src/common/string_interner.rs
@@ -505,8 +505,8 @@ pub struct Stats {
 /// header records at intern time.
 ///
 /// # Example
-/// ```rust
-/// use crate::common::string_interner::InternedString;
+/// ```ignore
+/// use valkey_timeseries::common::string_interner::InternedString;
 ///
 /// let x = InternedString::new("hello");
 /// let y: InternedString = "world".into();
