ae9b64d239432a142212bfcf1ab33b2c631fd4e4
ccollie <gboyah@gmail.com>
2026-05-16 13:19:34 -0400

    refactor(stale_ids): improved stale ID cleanup process with cursor management

--- src/series/tasks/mod.rs
+++ src/series/tasks/mod.rs
@@ -5,4 +5,6 @@ mod utils;
 
 pub(in crate::series) use optimize_indices::optimize_indices_for_db;
 pub(in crate::series) use series_trim::process_series_trim;
-pub(in crate::series) use stale_ids::{remove_stale_series_ids, remove_stale_series_internal};
+pub(in crate::series) use stale_ids::{
+    remove_all_stale_series_internal, remove_stale_series_ids_incremental,
+};
