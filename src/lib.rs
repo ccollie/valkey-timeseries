17427bbaba3bdec21572414182baa188c3bdea70
ccollie <gboyah@gmail.com>
2026-06-11 09:24:27 -0400

    Fix allocator configuration for test and doctest conditions

--- src/lib.rs
+++ src/lib.rs
@@ -235,7 +235,7 @@ fn shutdown_event_handler(ctx: &Context, _event: u64) {
     IS_SHUTTING_DOWN.store(true, Ordering::Relaxed);
 }
 
-#[cfg(not(all(test, doctest)))]
+#[cfg(not(any(test, doctest)))]
 macro_rules! get_allocator {
     () => {
         // Not `ValkeyAlloc` directly: it ignores `Layout::align()`, returning
@@ -244,7 +244,7 @@ macro_rules! get_allocator {
     };
 }
 
-#[cfg(all(test, doctest))]
+#[cfg(any(test, doctest))]
 macro_rules! get_allocator {
     () => {
         std::alloc::System
