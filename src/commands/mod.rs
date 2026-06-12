8068ea9675432c470907de1c2d9c11562d6fc05c
ccollie <gboyah@gmail.com>
2026-06-12 11:42:23 -0400

    feat: add `TS.STATIONARITY` to test whether a time series is stationary.

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -79,6 +79,7 @@ mod ts_querylabels_fanout_command;
 mod ts_range;
 mod ts_read;
 mod ts_restore;
+mod ts_stationarity;
 mod ts_trend;
 mod utils;
 
@@ -97,6 +98,7 @@ pub use ts_features::*;
 pub use ts_mget::*;
 pub use ts_restore::*;
 pub use ts_periods::*;
+pub use ts_stationarity::*;
 pub use ts_trend::*;
 use valkey_module::ValkeyResult;
 
