0c1c7443d7f69a51ddc2faa97fed0f6cc0af0a47
ccollie <gboyah@gmail.com>
2026-07-22 21:20:02 -0400

    Remove unimplemented "allocation" benchmark

--- benches/allocations.rs
+++
@@ -1,8 +0,0 @@
-use criterion::{Criterion, criterion_group, criterion_main};
-
-fn allocations_smoke(c: &mut Criterion) {
-    c.bench_function("allocations/smoke", |b| b.iter(|| 4usize + 4));
-}
-
-criterion_group!(benches, allocations_smoke);
-criterion_main!(benches);
